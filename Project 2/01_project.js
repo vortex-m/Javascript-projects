f3bec11965cb9709ffdfb764f420757681e3d3bf
Mayank <mail.mayank001@gmail.com>
2024-03-24 17:43:52 +0530

    First commit

--- Project 2/01_project.js
+++ Project 2/01_project.js
@@ -20,17 +20,17 @@ form.addEventListener('submit', (e) => {
         results.innerHTML = `Please give a valid weight ${weight}`;
     }
     else {
-       const val = (weight / ((height * height) / 10000)).toFixed(2)
+        const val = (weight / ((height * height) / 10000)).toFixed(2)
 
-       if(val < 18.6){
-        results.innerHTML = `You are Under weight and your BMI is ${val}`;
-       }
-       else if(val >= 18.6 && val <= 24.9){
-        results.innerHTML = `You are normal range and your BMI is ${val}`;
-       }
-       else if(val > 24.9){
-        results.innerHTML = `You are over weight and your BMI is ${val}`;
-       }
+        if (val < 18.6) {
+            results.innerHTML = `You are Under weight and your BMI is ${val}`;
+        }
+        else if (val >= 18.6 && val <= 24.9) {
+            results.innerHTML = `You are normal range and your BMI is ${val}`;
+        }
+        else if (val > 24.9) {
+            results.innerHTML = `You are over weight and your BMI is ${val}`;
+        }
     }
 
 })
